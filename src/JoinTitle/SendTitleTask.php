25f0742c2aa2d764982443473524194ea819fd60
Enrick3344 <enrico3815@hotmail.fr>
2017-11-02 19:10:07 -0400

    Make SendTitleTask.php

---
+++ src/JoinTitle/SendTitleTask.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace JoinTitle;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class SendTitleTask extends PluginTask{
+    
+    private $plugin;
+    
+    public function __construct(Main $plugin, Player $player){
+		    parent::__construct($plugin, $player);
+		    $this->plugin = $plugin;
+	  }
+    
+    public function onRun(int $currentTick){
+        $title = $this->plugin->getConfig()->get("Title");
+        $subtitle = $this->plugin->getConfig()->get("Subtitle");
+        $fadein = $this->plugin->getConfig()->get("Fade-In");
+        $lenght = $this->plugin->getConfig()->get("Lenght");
+        $fadeout = $this->plugin->getConfig()->get("Fade-Out");
+        $player->addTitle($title, $subtitle, $fadein, $lenght, $fadeout);
+    }
+}
