0872864faacc8cdd1413fa48e76447fbf5167617
Enrick3344 <enrico3815@hotmail.fr>
2017-11-02 19:01:50 -0400

    Make Main.php

---
+++ src/JoinTitle/Main.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace JoinTitle;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
+  
+  public function onEnable(){
+      @mkdir($this->getDataFolder());
+      $this->saveDefaultConfig();
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      $this->getServer()->getLogger()->notice("JoinTitle by Enrick3344 Enabled!");
+  }
+  
+  public function translateColors($string){
+		$msg = str_replace("&1",TextFormat::DARK_BLUE,$string);
+		$msg = str_replace("&2",TextFormat::DARK_GREEN,$msg);
+		$msg = str_replace("&3",TextFormat::DARK_AQUA,$msg);
+		$msg = str_replace("&4",TextFormat::DARK_RED,$msg);
+		$msg = str_replace("&5",TextFormat::DARK_PURPLE,$msg);
+		$msg = str_replace("&6",TextFormat::GOLD,$msg);
+		$msg = str_replace("&7",TextFormat::GRAY,$msg);
+		$msg = str_replace("&8",TextFormat::DARK_GRAY,$msg);
+		$msg = str_replace("&9",TextFormat::BLUE,$msg);
+		$msg = str_replace("&0",TextFormat::BLACK,$msg);
+		$msg = str_replace("&a",TextFormat::GREEN,$msg);
+		$msg = str_replace("&b",TextFormat::AQUA,$msg);
+		$msg = str_replace("&c",TextFormat::RED,$msg);
+		$msg = str_replace("&d",TextFormat::LIGHT_PURPLE,$msg);
+		$msg = str_replace("&e",TextFormat::YELLOW,$msg);
+		$msg = str_replace("&f",TextFormat::WHITE,$msg);
+		$msg = str_replace("&o",TextFormat::ITALIC,$msg);
+		$msg = str_replace("&l",TextFormat::BOLD,$msg);
+		$msg = str_replace("&r",TextFormat::RESET,$msg);
+		return $msg;
+	}
+  
+  public function onJoin(PlayerJoinEvent $event){
+      $player = $event->getPlayer();
+      if($this->getConfig()->get("Title-on-join") == true){
+          $this->getServer()->getScheduler()->scheduleDelayedTask(new SendTitleTask($this, $player), 30);
+      }
+  }
+}
